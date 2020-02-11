7dea719770e2cd75df817b0790c58c854fd0e09b
asmaafayed02 <asmaafayed02@gmail.com>
2020-02-11 04:05:05 +0200

    tabs with anther logic

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 //define variable
-const jsBtn = document.getElementById('js'),
+let  jsBtn = document.getElementById('js'),
       phpBtn = document.getElementById('php'),
       rubyBtn = document.getElementById('ruby'),
       messageDiv = document.querySelector('.message');
-    
+ /*   
  //add event to js button     
 jsBtn.onclick = function(){
     //the inner message
@@ -32,4 +32,52 @@ rubyBtn.onclick = function(){
     //remove class active from js and php button
     jsBtn.removeAttribute('class','active');
     phpBtn.removeAttribute('class','active')
+}*/
+//anther logic
+/*
+function changeBtn(language){
+    messageDiv.innerHTML = language + ' is great';
+    if(language == 'javaScript'){
+          //add class active to js button
+    jsBtn.setAttribute('class','active');
+    //remove class active from php and ruby button
+    phpBtn.removeAttribute('class','active');
+    rubyBtn.removeAttribute('class','active');
+    } else if(language == 'php'){
+        //add class active to php button
+    phpBtn.setAttribute('class','active');
+    //remove class active from js and ruby button
+    jsBtn.removeAttribute('class','active');
+    rubyBtn.removeAttribute('class','active');
+    
+    }if(language == 'ruby'){
+         //add class active to ruby button
+    rubyBtn.setAttribute('class','active');
+    //remove class active from js and php button
+    jsBtn.removeAttribute('class','active');
+    phpBtn.removeAttribute('class','active')
+    }
 }
+
+
+
+jsBtn.addEventListener('click',()=>{changeBtn('javaScript')})
+phpBtn.addEventListener('click',()=>{changeBtn('php')})
+rubyBtn.addEventListener('click',()=>{changeBtn('ruby')})*/
+
+//anther logic
+//function to add class to button and remove from anther
+function changeBtn(nameBtn,language,firstBtn,secondBtn){
+    messageDiv.innerHTML = language + ' is great'; //message inside div 
+    nameBtn.setAttribute('class','active');//add class to button 
+    firstBtn.removeAttribute('class','active');//remove class from anther button
+    secondBtn.removeAttribute('class','active')
+
+   }
+   //add event to first button
+jsBtn.addEventListener('click',()=>{changeBtn(jsBtn,'javascript',phpBtn,rubyBtn)})
+   //add event to second button
+phpBtn.addEventListener('click',()=>{changeBtn(phpBtn,'php',jsBtn,rubyBtn)})
+   //add event to third button
+rubyBtn.addEventListener('click',()=>{changeBtn(rubyBtn,'ruby',jsBtn,phpBtn)})
+
